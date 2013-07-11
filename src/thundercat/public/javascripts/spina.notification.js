1b7b8d865b201e8b0cc5f7dc713745cba4b5a8ce
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2013-07-11 20:28:09 +0100

    removed deadweight

--- src/thundercat/public/javascripts/spina.notification.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-  $(document).on('click', 'a[data-dismiss="notification"]', function() {
-    return $(this).parent('.notification').fadeOut(200, function() {
-      return $(this).remove();
-    });
-  });
-
-}).call(this);
