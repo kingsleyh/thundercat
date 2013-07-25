8e893fedfd2abc200499d6459cfdf610e95b3926
Kingsley Hendrickse <kings@Electron.local>
2013-07-25 22:41:03 +0300

    fixed some tests

--- spec/web_app_status_spec.rb
+++ spec/web_app_status_spec.rb
@@ -5,7 +5,7 @@ describe "IntegrationTests" do
 
   it 'should list all the available apps in the webapps directory' do
     webapps = File.dirname(File.expand_path(__FILE__)) + '/../integration/webapps'
-    WebAppStatus.new(webapps).discover.should == [{:pid_data => [], :has_pids => "no", :webapp => "sinatra_app_1", :location => "/Users/kings/development/projects/thundercat/spec/../integration/webapps/sinatra_app_1", :id => 0, :name => "My Sinatra App", :version => "0.0.1", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}, {:pid_data => [], :has_pids => "no", :webapp => "sinatra_app_2", :location => "/Users/kings/development/projects/thundercat/spec/../integration/webapps/sinatra_app_2", :id => 1, :name => "My Sinatra App", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}]
+    validate_maps(WebAppStatus.new(webapps).discover, [{:pid_data => [], :has_pids => "no", :webapp => "sinatra_app_1", :location => "/Users/kings/development/projects/thundercat/spec/../integration/webapps/sinatra_app_1", :id => 0, :name => "My Sinatra App", :version => "0.0.1", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}, {:pid_data => [], :has_pids => "no", :webapp => "sinatra_app_2", :location => "/Users/kings/development/projects/thundercat/spec/../integration/webapps/sinatra_app_2", :id => 1, :name => "My Sinatra App", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}])
   end
 
   it 'should list all the available apps in the webapps directory as json' do
@@ -57,8 +57,20 @@ describe "UnitTests" do
     pid_info.should_receive(:new).with('webapps_dir/app2/tmp/pids').and_return(pid_info_instance_2)
     pid_info_instance_1.should_receive(:discover).and_return({:pid_data => ['1111', '2222']})
     pid_info_instance_2.should_receive(:discover).and_return({:pid_data => ['3333', '4444']})
-    WebAppStatus.new('webapps_dir', file, pid_info, dir, yaml).discover.should == [{:pid_data => ["1111", "2222"], :webapp => "app1", :location => "webapps_dir/app1", :id => 0, :name => "My Sinatra App 1", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}, {:pid_data => ["3333", "4444"], :webapp => "app2", :location => "webapps_dir/app2", :id => 1, :name => "ThunderCat", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "ThunderCat", :is_thundercat => "yes"}]
+    validate_maps(WebAppStatus.new('webapps_dir', file, pid_info, dir, yaml).discover,[{:pid_data => ["1111", "2222"], :webapp => "app1", :location => "webapps_dir/app1", :id => 0, :name => "My Sinatra App 1", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}, {:pid_data => ["3333", "4444"], :webapp => "app2", :location => "webapps_dir/app2", :id => 1, :name => "ThunderCat", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "ThunderCat", :is_thundercat => "yes"}])
   end
 
 
+end
+
+def validate_maps(actual_map_list, expected_map_list)
+  actual_map_list.each_with_index do |map,i|
+    validate_map(map,expected_map_list[i])
+  end
+end
+
+def validate_map(actual_map, expected_map)
+  actual_map.each do |key,value|
+    expected_map[key].should == value
+  end
 end
--- thundercat.gemspec
+++ thundercat.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = "thundercat"
-  s.version = "0.0.4"
+  s.version = "0.0.5"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Kingsley Hendrickse"]
@@ -43,6 +43,7 @@ Gem::Specification.new do |s|
       s.add_runtime_dependency(%q<rake>, [">= 0"])
       s.add_runtime_dependency(%q<sys-proctable>, [">= 0"])
       s.add_runtime_dependency(%q<jeweler>, [">= 0"])
+      s.add_runtime_dependency(%q<rappa>, [">= 0"])
     else
       s.add_dependency(%q<sinatra>, [">= 0"])
       s.add_dependency(%q<sinatra-formhelpers>, [">= 0"])
@@ -55,6 +56,7 @@ Gem::Specification.new do |s|
       s.add_dependency(%q<rake>, [">= 0"])
       s.add_dependency(%q<sys-proctable>, [">= 0"])
       s.add_dependency(%q<jeweler>, [">= 0"])
+      s.add_runtime_dependency(%q<rappa>, [">= 0"])
     end
   else
     s.add_dependency(%q<sinatra>, [">= 0"])
@@ -68,6 +70,7 @@ Gem::Specification.new do |s|
     s.add_dependency(%q<rake>, [">= 0"])
     s.add_dependency(%q<sys-proctable>, [">= 0"])
     s.add_dependency(%q<jeweler>, [">= 0"])
+    s.add_runtime_dependency(%q<rappa>, [">= 0"])
   end
 end
 
