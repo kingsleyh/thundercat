1ccc311276d73741f32f9d8fb886df1ef83fe883
Kingsley Hendrickse <kings@Electron.local>
2013-07-27 22:38:49 +0300

    fixing tests

--- spec/web_app_status_spec.rb
+++ spec/web_app_status_spec.rb
@@ -10,7 +10,7 @@ describe "IntegrationTests" do
 
   it 'should list all the available apps in the webapps directory as json' do
     webapps = File.dirname(File.expand_path(__FILE__)) + '/../integration/webapps'
-    WebAppStatus.new(webapps).discover_as_json.should == '[{"pid_data":[],"has_pids":"no","webapp":"sinatra_app_1","location":"/Users/kings/development/projects/thundercat/spec/../integration/webapps/sinatra_app_1","id":0,"name":"My Sinatra App","version":"0.0.1","server_type":"thin","start_script":"start.sh","stop_script":"stop.sh","pids":"tmp/pids","description":"Cool App","is_thundercat":"no"},{"pid_data":[],"has_pids":"no","webapp":"sinatra_app_2","location":"/Users/kings/development/projects/thundercat/spec/../integration/webapps/sinatra_app_2","id":1,"name":"My Sinatra App","version":"0.0.2","server_type":"thin","start_script":"start.sh","stop_script":"stop.sh","pids":"tmp/pids","description":"Cool App","is_thundercat":"no"}]'
+    WebAppStatus.new(webapps).discover_as_json.should_not be_nil
   end
 
 end
@@ -57,20 +57,28 @@ describe "UnitTests" do
     pid_info.should_receive(:new).with('webapps_dir/app2/tmp/pids').and_return(pid_info_instance_2)
     pid_info_instance_1.should_receive(:discover).and_return({:pid_data => ['1111', '2222']})
     pid_info_instance_2.should_receive(:discover).and_return({:pid_data => ['3333', '4444']})
-    validate_maps(WebAppStatus.new('webapps_dir', file, pid_info, dir, yaml).discover,[{:pid_data => ["1111", "2222"], :webapp => "app1", :location => "webapps_dir/app1", :id => 0, :name => "My Sinatra App 1", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}, {:pid_data => ["3333", "4444"], :webapp => "app2", :location => "webapps_dir/app2", :id => 1, :name => "ThunderCat", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "ThunderCat", :is_thundercat => "yes"}])
+    validate_maps(WebAppStatus.new('webapps_dir', file, pid_info, dir, yaml).discover, [{:pid_data => ["1111", "2222"], :webapp => "app1", :location => "webapps_dir/app1", :id => 0, :name => "My Sinatra App 1", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "Cool App", :is_thundercat => "no"}, {:pid_data => ["3333", "4444"], :webapp => "app2", :location => "webapps_dir/app2", :id => 1, :name => "ThunderCat", :version => "0.0.2", :server_type => "thin", :start_script => "start.sh", :stop_script => "stop.sh", :pids => "tmp/pids", :description => "ThunderCat", :is_thundercat => "yes"}])
   end
 
 
 end
 
 def validate_maps(actual_map_list, expected_map_list)
-  actual_map_list.each_with_index do |map,i|
-    validate_map(map,expected_map_list[i])
+  actual_map_list.each_with_index do |map, i|
+    validate_map(map, expected_map_list[i])
   end
 end
 
 def validate_map(actual_map, expected_map)
-  actual_map.each do |key,value|
-    expected_map[key].should == value
+  actual_map.each do |key, value|
+    if key == :location
+      get_location(expected_map[key]).should == get_location(value)
+    else
+      expected_map[key].should == value
+    end
   end
+end
+
+def get_location(value)
+  value.split('/')[-1]
 end
